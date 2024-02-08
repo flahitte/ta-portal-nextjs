b97cd1d1d3f0ea1bbe55ef8bde9c01d5fdfab76c
Florian <florian.lahitte@uvltd.tech>
2024-02-08 15:25:43 -0500

    init nextAuth with okta

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+// imports
+import NextAuth from "next-auth"
+
+// importing providers
+// import GithubProvider from "next-auth/providers/github"
+import OktaProvider from "next-auth/providers/okta";
+
+const handler = NextAuth({
+    providers: [
+        // GithubProvider({
+        //     clientId: process.env.GITHUB_ID as string,
+        //     clientSecret: process.env.GITHUB_SECRET as string,
+        // }),
+        OktaProvider({
+            clientId: process.env.OKTA_OAUTH2_CLIENT_ID as string,
+            clientSecret: process.env.OKTA_OAUTH2_CLIENT_SECRET as string,
+            issuer: process.env.OKTA_OAUTH2_ISSUER as string
+          }),
+    ]
+})
+
+export { handler as GET, handler as POST }
